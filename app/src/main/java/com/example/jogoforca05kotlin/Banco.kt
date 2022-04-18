62105af459facb7363e92b612f0be1f009867621
lAlissial <alissialima@hotmail.com>
2022-04-17 23:06:27 -0300

    add: novas palavras

--- app/src/main/java/com/example/jogoforca05kotlin/Banco.kt
+++ app/src/main/java/com/example/jogoforca05kotlin/Banco.kt
@@ -15,6 +15,12 @@ class Banco {
         this.dicionario_palavras.put("LIVRARIA","Tem muitos livros")
         this.dicionario_palavras.put("PUDIM","Sobremesa")
         this.dicionario_palavras.put("NAVE","Veiculo também utilizado por aliens")
+        this.dicionario_palavras.put("ROXO","Azul + Vermelho")
+        this.dicionario_palavras.put("BOLO","Aniversário")
+        this.dicionario_palavras.put("NOITE","Antônimo: dia")
+        this.dicionario_palavras.put("FACA","Cortante")
+        this.dicionario_palavras.put("MORANGO","Pseudofruto vermelho")
+        this.dicionario_palavras.put("GELO","Água em estado sólido")
     }
 
     fun sorteia(){
